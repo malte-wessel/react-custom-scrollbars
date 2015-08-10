09805b828529ed43e84cfb79e5d46869308e52ff
Malte W <wessel@rheinschafe.de>
2015-08-10 13:42:23 +0200

    remove file

--- src/utils/addStylesheet.js
+++
@@ -1,8 +0,0 @@
-export default function addStyleSheet(styles) {
-    if (document.getElementById('react-custom-scrollbars-styles')) return;
-    const style = document.createElement('style');
-    style.type = 'text/css';
-    style.id = 'react-custom-scrollbars-styles';
-    style.innerHTML = styles;
-    document.body.appendChild(style);
-}
