88bde1cc73ecbd8950a562397532320b3b69332f
Malte W <wessel@rheinschafe.de>
2016-01-10 12:30:18 +0100

    updated example

--- examples/simple/webpack.config.js
+++ examples/simple/webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
     devtool: 'eval',
     entry: entry.concat('./index'),
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'static'),
         filename: 'bundle.js',
         publicPath: '/static/'
     },
