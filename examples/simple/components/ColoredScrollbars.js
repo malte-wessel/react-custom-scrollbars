3101e664d0f1ecda6fe23ab060f76a21733c0d4a
Malte W <wessel@rheinschafe.de>
2016-01-10 10:08:04 +0100

    updated example

---
+++ examples/simple/components/ColoredScrollbars.js
@@ -0,0 +1,39 @@
+import React, { createClass } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
+
+export default createClass({
+
+    displayName: 'ColoredScrollbars',
+
+    getInitialState() {
+        return {
+            top: 0
+        };
+    },
+
+    handleScroll(event, values) {
+        const { top } = values;
+        this.setState({ top });
+    },
+
+    render() {
+        const { top } = this.state;
+
+        const viewStyle = {
+            backgroundColor: `rgb(${Math.round(255 - (top * 255))}, ${Math.round(top * 255)}, ${Math.round(255)})`,
+            color: `rgb(${Math.round(255 - (top * 255))}, ${Math.round(255 - (top * 255))}, ${Math.round(255 - (top * 255))})`
+        };
+        const thumbStyle = {
+            backgroundColor: `rgb(${Math.round(255 - (top * 255))}, ${Math.round(255 - (top * 255))}, ${Math.round(255 - (top * 255))})`
+        };
+
+        return (
+            <Scrollbars
+                view={({style, ...props}) => <div className="box" style={{...style, ...viewStyle}} {...props}/>}
+                thumbHorizontal={({style, ...props}) => <div style={{...style, ...thumbStyle}} {...props}/>}
+                thumbVertical={({style, ...props}) => <div style={{...style, ...thumbStyle}} {...props}/>}
+                onScroll={this.handleScroll}
+                {...this.props}/>
+        );
+    }
+});
