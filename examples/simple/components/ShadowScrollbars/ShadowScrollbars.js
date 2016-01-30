3b2f9bcf1c2129da90b16d5865a694996d306dc5
Malte W <wessel@rheinschafe.de>
2016-01-31 00:06:09 +0100

    added shadow example

---
+++ examples/simple/components/ShadowScrollbars/ShadowScrollbars.js
@@ -0,0 +1,61 @@
+import React, { createClass, PropTypes } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
+
+export default createClass({
+
+    displayName: 'ShadowScrollbars',
+
+    propTypes: {
+        style: PropTypes.object
+    },
+
+    getInitialState() {
+        return {
+            scrollTop: 0,
+            scrollHeight: 0,
+            clientHeight: 0
+        };
+    },
+
+    handleScroll(event, values) {
+        const { scrollTop, scrollHeight, clientHeight } = values;
+        this.setState({ scrollTop, scrollHeight, clientHeight });
+    },
+
+    render() {
+        const { style } = this.props;
+        const { scrollTop, scrollHeight, clientHeight } = this.state;
+        const containerStyle = {
+            ...style,
+            position: 'relative'
+        };
+        const shadowTopStyle = {
+            opacity: (scrollTop - 20) / 20,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            height: 10,
+            background: 'linear-gradient(to bottom, rgba(0,0,0,0.2) 0%,rgba(0,0,0,0) 100%)'
+        };
+        const shadowBottomStyle = {
+            opacity: (scrollHeight - clientHeight - scrollTop - 20) / 20,
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 10,
+            background: 'linear-gradient(to top, rgba(0,0,0,0.2) 0%,rgba(0,0,0,0) 100%)'
+        };
+
+        return (
+            <div style={containerStyle}>
+                <Scrollbars
+                    onScroll={this.handleScroll}
+                    {...this.props}/>
+                <div style={shadowTopStyle}/>
+                <div style={shadowBottomStyle}/>
+            </div>
+        );
+    }
+});
