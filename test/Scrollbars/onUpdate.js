0bddf050c3fea4cdaf3908f250210720fcb8b201
Malte W <wessel@rheinschafe.de>
2016-03-16 11:11:02 +0100

    wrap onUpdate tests

--- test/Scrollbars/onUpdate.js
+++ test/Scrollbars/onUpdate.js
@@ -13,51 +13,53 @@ export default function createTests() {
         document.body.removeChild(node);
     });
 
-    describe('when scrolling x-axis', () => {
-        it('should call `onUpdate`', done => {
-            const spy = createSpy();
-            render((
-                <Scrollbars style={{ width: 100, height: 100 }} onUpdate={spy}>
-                    <div style={{ width: 200, height: 200 }}/>
-                </Scrollbars>
-            ), node, function callback() {
-                this.scrollLeft(50);
-                setTimeout(() => {
-                    expect(spy.calls.length).toEqual(1);
-                    done();
-                }, 100);
+    describe('onUpdate', () => {
+        describe('when scrolling x-axis', () => {
+            it('should call `onUpdate`', done => {
+                const spy = createSpy();
+                render((
+                    <Scrollbars style={{ width: 100, height: 100 }} onUpdate={spy}>
+                        <div style={{ width: 200, height: 200 }}/>
+                    </Scrollbars>
+                ), node, function callback() {
+                    this.scrollLeft(50);
+                    setTimeout(() => {
+                        expect(spy.calls.length).toEqual(1);
+                        done();
+                    }, 100);
+                });
             });
         });
-    });
-    describe('when scrolling y-axis', () => {
-        it('should call `onUpdate`', done => {
-            const spy = createSpy();
-            render((
-                <Scrollbars style={{ width: 100, height: 100 }} onUpdate={spy}>
-                    <div style={{ width: 200, height: 200 }}/>
-                </Scrollbars>
-            ), node, function callback() {
-                this.scrollTop(50);
-                setTimeout(() => {
-                    expect(spy.calls.length).toEqual(1);
-                    done();
-                }, 100);
+        describe('when scrolling y-axis', () => {
+            it('should call `onUpdate`', done => {
+                const spy = createSpy();
+                render((
+                    <Scrollbars style={{ width: 100, height: 100 }} onUpdate={spy}>
+                        <div style={{ width: 200, height: 200 }}/>
+                    </Scrollbars>
+                ), node, function callback() {
+                    this.scrollTop(50);
+                    setTimeout(() => {
+                        expect(spy.calls.length).toEqual(1);
+                        done();
+                    }, 100);
+                });
             });
         });
-    });
 
-    describe('when resizing window', () => {
-        it('should call onUpdate', done => {
-            const spy = createSpy();
-            render((
-                <Scrollbars style={{ width: 100, height: 100 }} onUpdate={spy}>
-                    <div style={{ width: 200, height: 200 }}/>
-                </Scrollbars>
-            ), node, function callback() {
-                setTimeout(() => {
-                    expect(spy.calls.length).toEqual(1);
-                    done();
-                }, 100);
+        describe('when resizing window', () => {
+            it('should call onUpdate', done => {
+                const spy = createSpy();
+                render((
+                    <Scrollbars style={{ width: 100, height: 100 }} onUpdate={spy}>
+                        <div style={{ width: 200, height: 200 }}/>
+                    </Scrollbars>
+                ), node, function callback() {
+                    setTimeout(() => {
+                        expect(spy.calls.length).toEqual(1);
+                        done();
+                    }, 100);
+                });
             });
         });
     });
