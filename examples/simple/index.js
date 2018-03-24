038f8ea0ea722c99eecb7ccdd177f1e1d29f6f44
Tatyana Tsvtekova <tsvettacsssr@yandex-team.ru>
2018-03-24 14:30:10 +0300

    add demos with horizontal scrollbars

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -6,8 +6,12 @@ import DefaultScrollbarsApp from './components/DefaultScrollbars/App';
 import ColoredScrollbarsApp from './components/ColoredScrollbars/App';
 import SpringScrollbarsApp from './components/SpringScrollbars/App';
 import ShadowScrollbarsApp from './components/ShadowScrollbars/App';
+import HorizontalScrollbarsApp from './components/HorizontalScrollbars/App';
+import BothScrollbarsApp from './components/BothScrollbars/App';
 
 render(<DefaultScrollbarsApp />, document.getElementById('default-scrollbars-root'));
 render(<ColoredScrollbarsApp />, document.getElementById('colored-scrollbars-root'));
 render(<SpringScrollbarsApp />, document.getElementById('spring-scrollbars-root'));
 render(<ShadowScrollbarsApp />, document.getElementById('shadow-scrollbars-root'));
+render(<HorizontalScrollbarsApp />, document.getElementById('horizontal-scrollbars-root'));
+render(<BothScrollbarsApp />, document.getElementById('both-scrollbars-root'));
