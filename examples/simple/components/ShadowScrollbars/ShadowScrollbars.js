e2252399d673cc8ce0296ef8a5151d774bc0af23
Muhaimin CS <muhaimincs@gmail.com>
2018-11-25 16:20:36 +0800

    remove unused state

--- examples/simple/components/ShadowScrollbars/ShadowScrollbars.js
+++ examples/simple/components/ShadowScrollbars/ShadowScrollbars.js
@@ -1,5 +1,5 @@
 import css from 'dom-css';
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 import { Scrollbars } from 'react-custom-scrollbars';
 
@@ -7,16 +7,14 @@ class ShadowScrollbars extends Component {
 
     constructor(props, ...rest) {
         super(props, ...rest);
-        this.state = {
-            scrollTop: 0,
-            scrollHeight: 0,
-            clientHeight: 0
-        };
-        this.handleUpdate = this.handleUpdate.bind(this);
+        this.state = {};
+        this.shadowTop = createRef();
+        this.shadowBottom = createRef();
     }
 
-    handleUpdate(values) {
-        const { shadowTop, shadowBottom } = this.refs;
+    handleUpdate = (values) => {
+        const shadowTop = this.shadowTop.current;
+        const shadowBottom = this.shadowBottom.current;
         const { scrollTop, scrollHeight, clientHeight } = values;
         const shadowTopOpacity = 1 / 20 * Math.min(scrollTop, 20);
         const bottomScrollTop = scrollHeight - clientHeight;
