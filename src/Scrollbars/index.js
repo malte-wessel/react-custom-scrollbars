02298eb9295cfbd5ee021c293573320641704a2b
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2019-12-12 10:39:54 -0800

    Fix firefox issues
    This removes code that has the firefox margins be -15px

--- src/Scrollbars/index.js
+++ src/Scrollbars/index.js
@@ -518,8 +518,8 @@ export default class Scrollbars extends Component {
         const viewStyle = {
             ...viewStyleDefault,
             // Hide scrollbars by setting a negative margin
-            marginRight: isFirefox ? '-15px' : 0,
-            marginBottom: isFirefox ? '-15px' : 0,
+            marginRight: 0,
+            marginBottom: 0,
             ...(autoHeight && {
                 ...viewStyleAutoHeight,
                 // Add scrollbarWidth to autoHeight in order to compensate negative margins
